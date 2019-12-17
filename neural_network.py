0019eb222679085b435584a0e4960f8c67f2a32e
joshualin24 <yaoyuyl2@illinois.edu>
2019-12-17 17:00:13 -0600

    training script ready

---
+++ neural_network.py
@@ -0,0 +1,218 @@
+#### 2019.12.17 at Office
+
+import torch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from  torch.autograd import Variable
+import torch.optim as optim
+from torchvision import datasets, transforms
+import torchvision.models as models
+import os, sys
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from tensorboardX import SummaryWriter
+import astropy
+from astropy.io import fits
+from astropy.table import Table
+import pandas as pd
+import csv
+import numpy as np
+import scipy as sp
+import scipy.ndimage
+import h5py
+from scipy.ndimage import gaussian_filter
+from scipy.ndimage import rotate
+import matplotlib.pyplot as plt
+import time
+import gc
+import datetime
+import matplotlib.pyplot as plt
+from astropy.coordinates import search_around_sky, SkyCoord
+from astropy import units as u
+from sklearn.model_selection import train_test_split
+
+
+
+root_folder = "/media/joshua/HDD_fun2/Public/"
+save_model_path = './saved_model/'
+
+
+EPOCH = 40
+glo_batch_size = 50
+test_num_batch = 50
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
+data_transform = transforms.Compose([
+            transforms.ToTensor(), # scale to [0,1] and convert to tensor
+            normalize,
+            ])
+target_transform = torch.Tensor
+
+
+class LensDataset(Dataset): # torch.utils.data.Dataset
+    def __init__(self, root_dir, train=True, transform=None, target_transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.target_transform = target_transform
+        self.train = train# training set or test set
+
+
+        if self.train:
+            self.path = root_dir#os.path.join(self.root_dir, self.train_folder)
+            self.df = pd.read_csv(self.path + '/train.csv')
+
+        else:
+            self.path = root_dir#os.path.join(self.root_dir, self.test_folder)
+            self.df = pd.read_csv(self.path + '/val.csv')
+
+    def __getitem__(self, index):
+
+        #print(self.df['ID'])
+        ID = self.df['ID'].iloc[[index]]
+        n_sources = self.df['n_sources'].iloc[[index]]
+        channel_names = ['EUC_H', 'EUC_J', 'EUC_Y', 'EUC_VIS']
+        # filepath = "/media/joshua/HDD_fun2/Public/EUC_Y/imageEUC_Y-" + str(ID.values[0]) + ".fits"
+        # lens_data = fits.open(filepath)
+        # img = lens_data[0].data
+        image = np.zeros((4, 224, 224))
+        try:
+            for i, channel in enumerate(channel_names):
+                filepath = self.path + channel + "/image" + channel + "-" + str(ID.values[0]) + ".fits"
+                lens_data = fits.open(filepath)
+                img = lens_data[0].data
+                img_channel_0 = scipy.ndimage.zoom(img, 224/img.shape[0], order=1)
+                image[i, :, :] += img_channel_0
+            #pass
+        except:
+            print("error", ID)
+            pass
+        #     plt.subplot(1, 4, i+ 1)
+        #     plt.imshow(image[i, :, :])
+        #     plt.title(channel + str(n_sources.values[0]))
+        # plt.show()
+
+        return image, n_sources.values[0]
+
+    def __len__(self):
+        return self.df.shape[0]
+
+
+train_loader = torch.utils.data.DataLoader(LensDataset(root_folder, train=True, transform=data_transform, target_transform=target_transform),
+                    batch_size = glo_batch_size, shuffle = True)
+
+if __name__ == '__main__':
+
+    if not os.path.exists(save_model_path):
+        os.mkdir(save_model_path)
+
+    dset_classes_number = 1
+    num_input_channel = 4
+    net = models.resnet18(pretrained=False)
+    net.conv1 = nn.Conv2d(num_input_channel, 64, kernel_size=7, stride=2, padding=3, bias=False)
+    num_ftrs = net.fc.in_features
+    #new_num_features = *something bigger than 512*
+    net.fc= nn.Linear(in_features=num_ftrs, out_features=dset_classes_number)
+    loss_fn = nn.BCEWithLogitsLoss(reduction='elementwise_mean')
+
+    net.cuda()
+
+    optimizer = optim.Adam(net.parameters(), lr = 1e-4)
+    tb = SummaryWriter()
+
+    best_accuracy = float("inf")
+
+    # for batch_idx, (data, n_sources) in enumerate(tqdm(train_loader, total = len(train_loader))):
+    #     data, target = data.float(), n_sources.float()
+    #     data, target = Variable(data).cuda(), Variable(target).cuda()
+    #     if batch_idx > 10:
+    #         break
+    for epoch in range(EPOCH):
+
+        net.train()
+        total_loss = 0.0
+        total_counter = 0
+        total_rms = 0
+
+        for batch_idx, (data, n_sources) in enumerate(tqdm(train_loader, total = len(train_loader))):
+            data, target = data.float(), n_sources.float()
+            data, target = Variable(data).cuda(), Variable(target).cuda()
+            data, target = data, target.unsqueeze(1)
+            #print("target:",target)
+            optimizer.zero_grad()
+            output = net(data)
+            #print("output:", output)
+            loss = loss_fn(output, target)
+
+            square_diff = (output - target) #((output - target)**2)**(0.5)
+            total_rms += square_diff.std(dim=0)
+            total_loss += loss.item()
+            total_counter += 1
+
+            loss.backward()
+            optimizer.step()
+            gc.collect()
+
+        # Collect RMS over each label
+        avg_rms = total_rms / (total_counter)
+        avg_rms = avg_rms.cpu()
+        avg_rms = (avg_rms.data).numpy()
+        for i in range(len(avg_rms)):
+            tb.add_scalar('rms %d' % (i+1), avg_rms[i])
+
+        # print test loss and tets rms
+        print(epoch, 'Train loss (averge per batch wise):', total_loss/(total_counter), ' RMS (average per batch wise):', np.array_str(avg_rms, precision=3))
+
+        with torch.no_grad():
+            net.eval()
+            total_loss = 0.0
+            total_counter = 0
+            total_rms = 0
+
+            test_loader = torch.utils.data.DataLoader(EHTDataset(root_folder, train=False, transform=data_transform, target_transform=target_transform),
+                        batch_size = glo_batch_size, shuffle = True
+                        )
+
+            # for batch_idx, (data, n_sources) in enumerate(tqdm(train_loader, total = len(train_loader))):
+            #     data, target = data.float(), mdot.float()
+            #     data, target = Variable(data).cuda(), Variable(target).cuda()
+            #     data, target = data, target.unsqueeze(1)
+            for batch_idx, (data, n_sources) in enumerate(test_loader):
+                data, target = data.float(), n_sources.float()
+                data, target = Variable(data).cuda(), Variable(target).cuda()
+                data, target = data, target.unsqueeze(1)
+
+
+
+
+                #pred [batch, out_caps_num, out_caps_size, 1]
+                pred = net(data)
+                loss = loss_fn(pred, target)
+                square_diff = (pred - target)
+                total_rms += square_diff.std(dim=0)
+                total_loss += loss.item()
+                total_counter += 1
+                gc.collect()
+
+                if batch_idx % test_num_batch == 0 and batch_idx != 0:
+                    tb.add_scalar('test_loss', loss.item())
+                    break
+
+            # Collect RMS over each label
+            avg_rms = total_rms / (total_counter)
+            avg_rms = avg_rms.cpu()
+            avg_rms = (avg_rms.data).numpy()
+            for i in range(len(avg_rms)):
+                tb.add_scalar('rms %d' % (i+1), avg_rms[i])
+
+            # print test loss and tets rms
+            print(epoch, 'Test loss (averge per batch wise):', total_loss/(total_counter), ' RMS (average per batch wise):', np.array_str(avg_rms, precision=3))
+            if total_loss/(total_counter) < best_accuracy:
+                best_accuracy = total_loss/(total_counter)
+                datetime_today = str(datetime.date.today())
+                torch.save(net, save_model_path + datetime_today + 'n_sources_' +'resnet18.mdl')
+                print("saved to " + "n_sources_resnet18.mdl" + " file.")
+
+tb.close()
---
+++ train_val_split.py
@@ -0,0 +1,39 @@
+import astropy
+from astropy.io import fits
+from astropy.table import Table
+import pandas as pd
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.coordinates import search_around_sky, SkyCoord
+from astropy import units as u
+from sklearn.model_selection import train_test_split
+from shutil import copyfile
+import os, sys
+
+
+
+#root_folder = "./v0812_redshift_aug_Full_train/"
+root_folder = "/media/joshua/HDD_fun2/Public/"
+
+#train_folder = "train/"
+#test_folder = "test/"
+full_data = pd.read_csv(root_folder + "image_catalog2.0train.csv" , skiprows=28)
+#full_data.to_csv("./Full_train/clean_full_data.csv")
+#print(full_data)
+
+# if not os.path.exists(root_folder + train_folder):
+#     os.mkdir(root_folder + train_folder)
+#
+# if not os.path.exists(root_folder + test_folder):
+#     os.mkdir(root_folder + test_folder)
+
+
+train, val =  train_test_split(full_data, test_size=0.2, random_state=42)
+
+#print(full_data.tail(), full_data.shape)
+print(train.head())
+print(val.shape)
+#
+train.to_csv(root_folder + "train.csv")
+val.to_csv(root_folder + "val.csv")
